98795e93b2826ac14acfaf6986c41661ea94a6bf
Dmytro Krutyi <krutiidmytro@gmail.com>
2026-04-01 20:29:10 +0200

    Change region

--- Dockerfile.php
+++ Dockerfile.php
@@ -7,4 +7,6 @@ COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
 RUN apk add --no-cache postgresql-dev libzip-dev icu-dev libsodium-dev \
     && docker-php-ext-install pdo_pgsql intl zip sodium opcache
 
+RUN printf "memory_limit=512M\n" > /usr/local/etc/php/conf.d/memory-limit.ini
+
 CMD ["php-fpm"]
